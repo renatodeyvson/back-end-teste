76f8246fb5087cf598e94c312f61a5eb97c3542e
renatodeyvson <renatodeyvson@gmail.com>
2019-04-11 21:28:52 -0300

    Add Brand and Photo Validation

--- src/controllers/phoneController.js
+++ src/controllers/phoneController.js
@@ -19,7 +19,7 @@ module.exports = {
         const { model,
           price,
           brand,
-          PushSubscriptionOptions,
+          photo,
           startDate,
           endDate,
           color,
@@ -27,16 +27,31 @@ module.exports = {
         } = req.body;
 
         const noSpaceModel = model.replace(/ /g, '');
+        const noSpaceBrand = brand.replace(/ /g, '');
+        const noSpacePhoto = photo.replace(/ /g, '');
+        console.log(noSpacePhoto.length);
         if(noSpaceModel.length < 2 || noSpaceModel.length > 255){
             return res.status(400).json({
                 message: 'Modelo inválido.',
-                description: 'O modelo deve ter no mínimo 2 e no máximo 255 caracteres, desprezando espaço em branco.'
+                description: 'O modelo deve ter no mínimo 2 e no máximo 255 caracteres, desprezando espaços em branco.'
             });
         }
         if(price < 0){
             return res.status(400).json({
                 message: 'Preço inválido.',
                 description: 'O preço deve ser um número positivo.'
+            });
+        }
+        if(noSpaceBrand.length < 2 || noSpaceBrand.length > 255){
+            return res.status(400).json({
+                message: 'Marca inválida.',
+                description: 'A marca deve ter no mínimo 2 e no máximo 255 caracteres, desprezando espaços em branco.'
+            });
+        }
+        if(noSpacePhoto.length > 255){
+            return res.status(400).json({
+                message: 'Foto inválida.',
+                description: 'A foto deve ter no máximo 255 caracteres, desprezando espaços em branco.'
             });
         }
         else{
