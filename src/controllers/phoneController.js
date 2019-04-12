d0f6ca21a3d810c07e46b7edde6f51be5cbf2874
renatodeyvson <renatodeyvson@gmail.com>
2019-04-11 21:05:45 -0300

    Add Model Validation

--- src/controllers/phoneController.js
+++ src/controllers/phoneController.js
@@ -16,9 +16,28 @@ module.exports = {
     },
 
     async add(req, res){
-        const phone = await Phone.create(req.body);
+        const { model,
+          price,
+          brand,
+          PushSubscriptionOptions,
+          startDate,
+          endDate,
+          color,
+          code
+        } = req.body;
 
-        return res.json(phone);
+        const noSpaceModel = model.replace(/ /g, '');
+        if(noSpaceModel.length < 2 || noSpaceModel.length > 255){
+            return res.status(400).json({
+                message: 'Modelo inválido.',
+                description: 'O modelo deve ter no mínimo 2 e no máximo 255 caracteres, desprezando espaço em branco.'
+            });
+        }
+        else{
+            const phone = await Phone.create(req.body);
+
+            return res.json(phone);
+        }
     },
 
     async upd(req, res){
