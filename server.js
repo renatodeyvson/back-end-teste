d989f8a08fcf5a711c71e98bd2480383bed67baf
renatodeyvson <renatodeyvson@gmail.com>
2019-04-12 14:43:43 -0300

    Enable CORS

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 //DB config
 const mongodbAddress = 'mongodb://melhoruser:melhorteste11@ds247852.mlab.com:47852/back-end-teste';
@@ -9,6 +10,7 @@ require('./src/models/Phone');
 
 //Server config
 app.use(express.json());
+app.use(cors());
 app.use('/api', require('./src/routes'));
 
 const port = process.env.PORT || 3000;
